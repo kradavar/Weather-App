e35d329a5a037f5dd11ca73110537662336e2b79
Darya Krasava <kradavar@gmail.com>
2018-09-25 20:27:17 +0300

    fetch the weather data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,32 @@ import './App.css';
 import WeatherDisplay from './WeatherDisplay';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: [
+        { name: "Palo Alto", zip: "94303" },
+        { name: "San Jose", zip: "94088" },
+        { name: "Santa Cruz", zip: "95062" },
+        { name: "Honolulu", zip: "96803" }],
+      activePlace: 0
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick(index) {
+    console.log("Clicked index " + index);
+    this.setState({ activePlace: index });
+  }
   render() {
     return (
       <div className="App">
-        <WeatherDisplay />
+        {this.state.places.map((city, index) => (
+          <button key={index} onClick={this.handleOnClick.bind(null, index)} >
+            {city.name}
+          </button>
+        ))}
+        <WeatherDisplay zip={"96803"} />
       </div>
     );
   }
--- src/WeatherDisplay.js
+++ src/WeatherDisplay.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react';
 
 class WeatherDisplay extends Component {
+  constructor() {
+    super();
+    this.state = {
+      weatherData: null
+    };
+  }
+
+  componentDidMount() {
+    const zip = this.props.zip;
+    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+      zip +
+      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial";
+    fetch(URL).then(res => res.json()).then(json => {
+      this.setState({
+        weatherData: json
+      });
+    });
+  }
+
   render() {
-    return (
-      <h1>Dispay some weather!</h1>
-    );
+    if (!this.state.weatherData) {
+      return (
+        <div>Loading</div>
+      );
+    } else {
+      const weather = this.state.weatherData.weather[0];
+      const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+      return (
+        <div>
+          <h1>
+            {weather.main} in {this.state.weatherData.name}
+            <img src={iconUrl} alt={this.state.weatherData.description} />
+          </h1>
+          <p>Current: {this.state.weatherData.main.temp}°</p>
+          <p>High: {this.state.weatherData.main.temp_max}°</p>
+          <p>Low: {this.state.weatherData.main.temp_min}°</p>
+          <p>Wind Speed: {this.state.weatherData.wind.speed} mi/hr</p>
+        </div>
+      );
+    }
   };
 }
 
