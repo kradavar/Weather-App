411e72ee54e39085071be8d3950602cc050c76f6
Darya Krasava <kradavar@gmail.com>
2018-09-25 21:33:08 +0300

    Add bootstrap to the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
+//                                                                      
 import WeatherDisplay from './WeatherDisplay';
 
 class App extends Component {
@@ -10,25 +11,55 @@ class App extends Component {
         { name: "Palo Alto", zip: "94303" },
         { name: "San Jose", zip: "94088" },
         { name: "Santa Cruz", zip: "95062" },
-        { name: "Honolulu", zip: "96803" }],
+        { name: "Honolulu", zip: "96803" },
+        { name: "Minsk", zip: "220000" }],
       activePlace: 0
     };
-    this.handleOnClick = this.handleOnClick.bind(this);
+    this.handleOnSelect = this.handleOnSelect.bind(this);
   }
 
-  handleOnClick(index) {
-    console.log("Clicked index " + index);
+  handleOnSelect(index) {
     this.setState({ activePlace: index });
   }
+
+  setCity() {
+
+  }
+
   render() {
+    const navItems = this.state.places.map((place, index) => (
+      <NavItem key={index} eventKey={index}>
+        {place.name}
+      </NavItem>
+    ));
     return (
       <div className="App">
-        {this.state.places.map((city, index) => (
-          <button key={index} onClick={this.handleOnClick.bind(null, index)} >
-            {city.name}
-          </button>
-        ))}
-        <WeatherDisplay zip={"96803"} />
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              React Simple Weather App
+          </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav bsStyle="pills"
+                stacked
+                activeKey={this.state.activePlace}
+                onSelect={index => {
+                  this.setState({ activePlace: index });
+                }}>
+                {navItems}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={this.state.activePlace}
+                zip={this.state.places[this.state.activePlace].zip} />
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
