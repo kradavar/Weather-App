083208e68f61d516f706da8529eac5aa9f5a8d69
Darya Krasava <kradavar@gmail.com>
2018-09-25 15:49:26 +0300

    Begin the app

---
+++ src/WeatherDisplay.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WeatherDisplay extends Component {
+  render() {
+    return (
+      <h1>Dispay some weather!</h1>
+    );
+  };
+}
+
+export default WeatherDisplay;
